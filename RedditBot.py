6780a7b6f8de26a3b851208e624e5fb876f7f136
Sc1F1 <malsky.john@gmail.com>
2014-05-03 18:55:20 -0500

    Added MAL API, fixed ParentCheck(), Deleted RedditLogin()
    Added MAL API, now fully functional.
    ParentCheck() no longer return error message/crashes program
    RedditLogin() was deleted as it was only run at the beginning, added
    it's function to CommentGet()(Still need to rewrite)

--- RedditBot.py
+++ RedditBot.py
@@ -1,5 +1,22 @@
+'''
+Reddit Anime Link Bot
+Copyright (C) 2014  John Malsky
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 import praw
-import urllib2
+import urllib2, base64
 
 ## Searches CrunchyRoll.com for an anime title, returns any it finds in a list.
 def CrunchyUrl(anime_titles):
@@ -17,7 +34,7 @@ def CrunchyUrl(anime_titles):
             if "<a href" in line and "\" class=\"clearfix\">" in line:
                 if count == 0:
                     crunchy_title = line.split("\"")[1]
-                    print crunchy_title
+                    #print crunchy_title
                     count += 1
         if crunchy_title == "":
             crunchy_url.append(False)
@@ -25,17 +42,36 @@ def CrunchyUrl(anime_titles):
             crunchy_url.append("http://crunchyroll.com%s" % crunchy_title)
     return crunchy_url
 
-## The only reason bot isn't currently running -.- waiting on MAL to send me a user agent
+## Returns a list of MAL urls
 def MALUrl(anime_titles):
+    username = 'PRIVATE'
+    password = 'PRIVATE'
+    MAL_urls = []
     for title in anime_titles:
-        url = ("http://myanimelist.net/api/anime/search.xml?q=%s" + title)
-        req = urllib2.Request(url, headers={'User-Agent': ""})
+        url = ("http://myanimelist.net/api/anime/search.xml?q=%s" % title.replace(" ", "+").lower())
+        req = urllib2.Request(url)
+        auth_string = base64.encodestring("%s:%s" % (username, password))
+        req.add_header("Authorization", "Basic %s" % auth_string)
 
         xml_req = urllib2.urlopen(req)
         xml_source = xml_req.readlines()
 
+        count = 0
         for line in xml_source:
-            print line
+            #print line
+            if "/id" in line and count == 0:
+                MAL_id = False
+                MAL_id = line.replace("<id>", "")
+                MAL_id = MAL_id.replace("</id>\n", "")
+                MAL_id = MAL_id.replace(" ", "")
+                #print MAL_id
+                count += 1
+                if MAL_id != False:
+                    MAL_urls.append("http://myanimelist.net/anime/%s" % MAL_id)
+                else:
+                    MAL_urls.append(False)
+
+    return MAL_urls
             
 ## Uses AnimeAdvice.me to check for similar anime.
 def GetSimilar(MAL_urls):
@@ -60,28 +96,23 @@ def GetSimilar(MAL_urls):
             similar_urls.append(False)
     return similar_urls
 
-## Login to Reddit
-def RedditLogin():
-    r = praw.Reddit('animelinkbot by /u/Sc1F1')
-    r.login('animelinkbot', 'PRIVATE')
-
-    return r
-
 ## Checks Anime title list for any matches in the comment, returns any it finds in a list
 def CheckAnimeTitles(comment):
-    anime_list = open("RedditBotAnimeList.txt", 'r')
+    ## Read in list of all anime(roughly 7300)
+    anime_list = open("RedditBotAnimeList.txt", 'r') 
     anime_title_list = anime_list.readlines()
     anime_list.close()
 
+    ## Check each anime against the comment. Unfortunately means people need to spell correctly/not use abbreviations. Will add synonyms to file later
     reply_title = []
     should_reply = False
     for title in anime_title_list:
         anime_title = title.replace("\n", "")
-        if unicode(anime_title, "utf-8") in comment:
+        ## Convert title to utf-8 first because that's what the comment is in
+        if unicode(anime_title, "utf-8") in comment and len(anime_title) > 1:## added len(title) to filter out false positives for one letter series e.g.('F', 'E')
             ##print anime_title
             reply_title.append(anime_title)
             should_reply = True
-        
 
     if should_reply == True:
         return reply_title
@@ -135,22 +166,31 @@ def CheckUrl(comment, url_list):
     return need_url
 
 ## Currently only checks the immediate parent, need to check all up to start of thread
-def CheckParent(comment, anime_titles, already_done):
+def CheckParent(r, comment, anime_titles, already_done):
     parent = r.get_info(thing_id=comment.parent_id)
     pass_check = []
-    for title in anime_titles:
-        if parent.id in already_done and title in parent.body:
-            pass_check.append(False)
-        elif title in parent.body and parent.author.name == 'animelinkbot':
-            pass_check.append(False)
-        else:
+    if isinstance(parent, praw.objects.Submission):
+        for title in anime_titles:
             pass_check.append(True)
+    else:
+        for title in anime_titles:
+            if parent.id in already_done and unicode(title, 'utf-8').lower() in parent.body.lower():
+                pass_check.append(False)
+            elif unicode(title,'utf-8').lower() in parent.body.lower() and parent.author.name == 'animelinkbot':
+                pass_check.append(False)
+            else:
+                pass_check.append(True)
     return pass_check
 
 
 ## Need to rewrite and split into seperate functions
-def CommentGet(r):
+## 
+def CommentGet():
+    
     already_done = set()
+    r = praw.Reddit('animelinkbot by /u/Sc1F1')
+    r.login('animelinkbot', 'PRIVATE')
+    
     while(1):
         multi_reddits = r.get_subreddit('animesuggest')
         multi_reddits_comments = multi_reddits.get_comments()
@@ -159,8 +199,8 @@ def CommentGet(r):
             if comment.id not in already_done and comment.author.name != 'animelinkbot':
                 already_done.add(comment.id)
                 anime_titles = CheckAnimeTitles(comment.body)
-                parent_check = CheckParent(comment, anime_titles, already_done)
                 if anime_titles != False:
+                    parent_check = CheckParent(r, comment, anime_titles, already_done)
                     MAL_urls = MALUrl(anime_titles)
                     crunchy_urls = CrunchyUrl(anime_titles)
                     need_MAL = CheckUrl(comment.body, MAL_urls)
@@ -170,8 +210,14 @@ def CommentGet(r):
                         similar_urls = GetSimilar(MAL_urls)
                         need_similar = CheckUrl(comment.body, similar_urls)
                         comment_reply = PrepareReply(anime_titles, MAL_urls, crunchy_urls, similar_urls, need_MAL, need_crunch, need_similar, parent_check)
+                        print comment_reply ## Mainly because I like seeing something in the console, all other print statements are for debug purposes
+                        '''
+                        try:
+                            comment.reply(comment_reply) ## tries to post reply(depending on karma, you may have to wait, so this would return an error)
+                        except:
+                            already_done.remove(comment.id) ## remove id from alread_done so it can try again in the future'''
                 
-
+## Starts at Hummingbirds newest entries and returns the title of every series in the database. 
 def GetAnimeListHummingbird():
     url_list = ["http://hummingbird.me/anime/filter/newest?y[]=Upcoming&y[]=2010s&y[]=2000s&y[]=1990s&y[]=1980s&y[]=1970s&y[]=Older"]
     anime_title = []
@@ -183,8 +229,7 @@ def GetAnimeListHummingbird():
         for line in html_source:
             if "<p class='title'>" in line:
                 line = line.replace("<p class='title'>", "")
-                line = line.replace("</p>", "")
-                line = line.replace("\n", "")
+                line = line.replace("</p>\n", "")
                 if line not in anime_title:
                     anime_title.append(line)
                     print line
@@ -194,7 +239,7 @@ def GetAnimeListHummingbird():
                 if anime_url not in url_list:
                     url_list.append(anime_url)
                     
-## Waiting on MAL API Whitelist acceptance
+## Going to do the same thing as above function
 def GetAnimeListMAL():
     url_list = ["http://myanimelist.net/anime.php?o=9&c[]=a&c[]=d&cv=2&w=1"]
     anime_title = []
