ffe829a0c25434b9b32d92bfe3b4e045949ff631
Sc1F1 <malsky.john@gmail.com>
2014-05-07 01:45:15 -0500

    Added CheckThread()
    Added CheckThread() function, which replaces CheckUrl(). It checks the
    entire thread for mentions of the url, and returns an list via the
    AnimeCheck() class. Values are boolean.

--- RedditBot.py
+++ RedditBot.py
@@ -18,29 +18,40 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import praw
 import urllib2, base64
 
+
+class AnimeCheck():
+    def __init__(self, MAL_pass, cr_pass):
+        self.MAL_pass = MAL_pass
+        self.cr_pass = cr_pass
+        
+
 ## Searches CrunchyRoll.com for an anime title, returns any it finds in a list.
 def CrunchyUrl(anime_titles):
     crunchy_url = []
     for title in anime_titles:
-        url = ("http://crunchyroll.com/search?q=%s" % title.replace(" ", "+").lower())
-        req = urllib2.Request(url, headers={'User-Agent': "Anime Browser"})
+        try:
+            url = ("http://crunchyroll.com/search?q=%s" % title.replace(" ", "+").lower())
+            req = urllib2.Request(url, headers={'User-Agent': "Anime Browser"})
 
-        html_req = urllib2.urlopen(req)
-        html_source = html_req.readlines()
+            html_req = urllib2.urlopen(req)
+            html_source = html_req.readlines()
     
-        count = 0
-        crunchy_title = ""
-        for line in html_source:
-            if "<a href" in line and "\" class=\"clearfix\">" in line:
-                if count == 0:
-                    crunchy_title = line.split("\"")[1]
-                    #print crunchy_title
-                    count += 1
-        if crunchy_title == "":
+            count = 0
+            crunchy_title = ""
+            for line in html_source:
+                if "<a href" in line and "\" class=\"clearfix\">" in line:
+                    if count == 0:
+                        crunchy_title = line.split("\"")[1]
+                        #print crunchy_title
+                        count += 1
+            if crunchy_title == "":
+                crunchy_url.append(False)
+            else:
+                crunchy_url.append("http://crunchyroll.com%s" % crunchy_title)
+        except:
             crunchy_url.append(False)
-        else:
-            crunchy_url.append("http://crunchyroll.com%s" % crunchy_title)
     return crunchy_url
+
 
 ## Returns a list of MAL urls
 def MALUrl(anime_titles):
@@ -48,53 +59,61 @@ def MALUrl(anime_titles):
     password = 'PRIVATE'
     MAL_urls = []
     for title in anime_titles:
-        url = ("http://myanimelist.net/api/anime/search.xml?q=%s" % title.replace(" ", "+").lower())
-        req = urllib2.Request(url)
-        auth_string = base64.encodestring("%s:%s" % (username, password))
-        req.add_header("Authorization", "Basic %s" % auth_string)
+        try:
+            url = ("http://myanimelist.net/api/anime/search.xml?q=%s" % title.replace(" ", "+").lower())
+            req = urllib2.Request(url)
+            auth_string = base64.encodestring("%s:%s" % (username, password))
+            req.add_header("Authorization", "Basic %s" % auth_string)
 
-        xml_req = urllib2.urlopen(req)
-        xml_source = xml_req.readlines()
+            xml_req = urllib2.urlopen(req)
+            xml_source = xml_req.readlines()
 
-        count = 0
-        for line in xml_source:
-            #print line
-            if "/id" in line and count == 0:
-                MAL_id = False
-                MAL_id = line.replace("<id>", "")
-                MAL_id = MAL_id.replace("</id>\n", "")
-                MAL_id = MAL_id.replace(" ", "")
-                #print MAL_id
-                count += 1
-                if MAL_id != False:
-                    MAL_urls.append("http://myanimelist.net/anime/%s" % MAL_id)
-                else:
-                    MAL_urls.append(False)
+            count = 0
+            for line in xml_source:
+                #print line
+                if "/id" in line and count == 0:
+                    MAL_id = False
+                    MAL_id = line.replace("<id>", "")
+                    MAL_id = MAL_id.replace("</id>\n", "")
+                    MAL_id = MAL_id.replace(" ", "")
+                    #print MAL_id
+                    count += 1
+                    if MAL_id != False:
+                        MAL_urls.append("http://myanimelist.net/anime/%s" % MAL_id)
+                    else:
+                        MAL_urls.append(False)
+        except:
+            MAL_urls.append(False)
 
     return MAL_urls
+
             
 ## Uses AnimeAdvice.me to check for similar anime.
 def GetSimilar(MAL_urls):
     index = 0
     similar_urls = []
     for url in MAL_urls:
-        anime_id = url.split("/")[4]
+        try:
+            anime_id = url.split("/")[4]
 
-        url = ("http://animeadvice.me/similarity/?name=id%s-anime" % (anime_id))
-        print url
-        req = urllib2.Request(url, headers={'User-Agent':'Anime Browser'})
+            url = ("http://animeadvice.me/similarity/?name=id%s-anime" % (anime_id))
+            ##print url
+            req = urllib2.Request(url, headers={'User-Agent':'Anime Browser'})
 
-        html_req = urllib2.urlopen(req)
-        html_source = html_req.readlines()
+            html_req = urllib2.urlopen(req)
+            html_source = html_req.readlines()
 
-        count = 0
-        for line in html_source:
-            if "http://myanimelist.net" in line and count == 0:
-                similar_urls.append(line.split("\"")[3])
-                count += 1
-        if count == 0:
+            count = 0
+            for line in html_source:
+                if "http://myanimelist.net" in line and count == 0:
+                    similar_urls.append(line.split("\"")[3])
+                    count += 1
+            if count == 0:
+                similar_urls.append(False)
+        except:
             similar_urls.append(False)
     return similar_urls
+
 
 ## Checks Anime title list for any matches in the comment, returns any it finds in a list
 def CheckAnimeTitles(comment):
@@ -119,103 +138,104 @@ def CheckAnimeTitles(comment):
     else:
         return False
     
+    
 ## Formats all information for a reply, only adds information if it needs to.
-def PrepareReply(anime_titles, MAL_urls, crunchy_urls, similar_urls, need_MAL, need_crunch, need_similar, parent_check):
+def PrepareReply(anime_titles, MAL_urls, crunchy_urls, similar_urls, need_MAL, need_crunch):
     comment_reply = "***"
     index = 0
     for title in anime_titles:
-        if parent_check != False:
-            comment_reply = comment_reply + ("\n\n* %s" % title)
-            if need_MAL[index] != False:
-                comment_reply = comment_reply + (" - [MAL](%s)" % MAL_urls[index])
-            if need_crunch[index] != False:
-                comment_reply = comment_reply + (" - [CR](%s)" % crunchy_urls[index])
-            if need_similar[index] != False and similar_urls[index] not in comment_reply:
-                comment_reply = comment_reply + (" - Similar Anime: [MAL](%s)" % similar_urls[index])
+        comment_reply = comment_reply + ("\n\n* %s" % title)
+        if need_MAL[index] != False:
+            comment_reply = comment_reply + (" - [MAL](%s)" % MAL_urls[index])
+        if need_crunch[index] != False:
+            comment_reply = comment_reply + (" - [CR](%s)" % crunchy_urls[index])
+        if similar_urls[index] not in comment_reply:
+            comment_reply = comment_reply + (" - Similar Anime: [MAL](%s)" % similar_urls[index])
         index += 1
     comment_reply = comment_reply + ("\n\n***\nPlease contact /u/Sc1F1 if you notice any bugs, incorrect links or have any questions.")
     return comment_reply
 
 
 ## If any URL returns true, Bot will reply
-def CheckReply(need_MAL, need_crunch, parent_check):
-    ##Both must be True for function to return True
+def CheckReply(need_MAL, need_crunch):
     need_reply = False
-    reply_needed = False
-    for value in parent_check:
+    for value in need_MAL:
         if value == True:
             need_reply = True
-    if need_reply == True:
-        for value in need_MAL:
-            if value == True:
-                reply_needed = True
-        for value in need_crunch:
-            if value == True:
-                reply_needed = True
+    for value in need_crunch:
+        if value == True:
+            need_reply = True
 
-    return reply_needed
+    return need_reply
 
-## Checks if url is in comment already
-def CheckUrl(comment, url_list):
-    need_url = []
-    for url in url_list:
-        if url == False or url in comment:
-            need_url.append(False)
-        else:
-            need_url.append(True)
-    return need_url
 
-## Currently only checks the immediate parent, need to check all up to start of thread
-def CheckParent(r, comment, anime_titles, already_done):
+## Checks entire thread for mentions of MAL or Crunchy urls
+def CheckThread(r, comment, MAL_urls, crunchy_urls):
     parent = r.get_info(thing_id=comment.parent_id)
     pass_check = []
+    is_submission = False
     if isinstance(parent, praw.objects.Submission):
-        for title in anime_titles:
-            pass_check.append(True)
+        is_submission = True
     else:
-        for title in anime_titles:
-            if parent.id in already_done and unicode(title, 'utf-8').lower() in parent.body.lower():
-                pass_check.append(False)
-            elif unicode(title,'utf-8').lower() in parent.body.lower() and parent.author.name == 'animelinkbot':
-                pass_check.append(False)
-            else:
-                pass_check.append(True)
-    return pass_check
+        while(is_submission == False):
+            parent = r.get_info(thing_id=parent.parent_id)
+            if isinstance(parent, praw.objects.Submission):
+                is_submission = True
 
+    try:
+        parent.replace_more_comments(limit=None, threshold=0)
+    except:
+        pass
+    
+    thread_comments = praw.helpers.flatten_tree(parent.comments)
+
+    MAL_pass = [True for x in range(len(MAL_urls))]
+    cr_pass = [True for x in range(len(crunchy_urls))]
+    for t_comment in thread_comments:
+        index = 0
+        for url in MAL_urls:
+            if url == False or url in t_comment.body:
+                MAL_pass[index] = False
+            index += 1
+        index = 0
+        for url in crunchy_urls:
+            if url == False or url in t_comment.body:
+                cr_pass[index] = False
+            index += 1
+
+    check = AnimeCheck(MAL_pass, cr_pass)
+    return check
+    
 
 ## Need to rewrite and split into seperate functions
-## 
 def CommentGet():
-    
     already_done = set()
     r = praw.Reddit('animelinkbot by /u/Sc1F1')
     r.login('animelinkbot', 'PRIVATE')
-    
     while(1):
         multi_reddits = r.get_subreddit('animesuggest')
         multi_reddits_comments = multi_reddits.get_comments()
-
         for comment in multi_reddits_comments:
             if comment.id not in already_done and comment.author.name != 'animelinkbot':
                 already_done.add(comment.id)
                 anime_titles = CheckAnimeTitles(comment.body)
                 if anime_titles != False:
-                    parent_check = CheckParent(r, comment, anime_titles, already_done)
                     MAL_urls = MALUrl(anime_titles)
                     crunchy_urls = CrunchyUrl(anime_titles)
-                    need_MAL = CheckUrl(comment.body, MAL_urls)
-                    need_crunch = CheckUrl(comment.body, crunchy_urls)
-                    need_reply = CheckReply(need_MAL, need_crunch, parent_check)
+                    check = CheckThread(r, comment, MAL_urls, crunchy_urls)
+                    need_MAL = check.MAL_pass
+                    need_crunch = check.cr_pass
+                    print MAL_urls, crunchy_urls, need_MAL, need_crunch
+                    need_reply = CheckReply(need_MAL, need_crunch)
                     if need_reply == True:
                         similar_urls = GetSimilar(MAL_urls)
-                        need_similar = CheckUrl(comment.body, similar_urls)
-                        comment_reply = PrepareReply(anime_titles, MAL_urls, crunchy_urls, similar_urls, need_MAL, need_crunch, need_similar, parent_check)
+                        comment_reply = PrepareReply(anime_titles, MAL_urls, crunchy_urls, similar_urls, need_MAL, need_crunch)
                         print comment_reply ## Mainly because I like seeing something in the console, all other print statements are for debug purposes
-                        '''
                         try:
-                            comment.reply(comment_reply) ## tries to post reply(depending on karma, you may have to wait, so this would return an error)
+                            comment.reply(comment_reply) ## tries to post reply(depending on karma, you may have to wait, so this would return an error
                         except:
-                            already_done.remove(comment.id) ## remove id from alread_done so it can try again in the future'''
+                            already_done.remove(comment.id) ## remove id from alread_done so it can try again in the future
+                            
                 
 ## Starts at Hummingbirds newest entries and returns the title of every series in the database. 
 def GetAnimeListHummingbird():
@@ -238,15 +258,3 @@ def GetAnimeListHummingbird():
                 anime_url = ("http://hummingbird.me" + anime_link)
                 if anime_url not in url_list:
                     url_list.append(anime_url)
-                    
-## Going to do the same thing as above function
-def GetAnimeListMAL():
-    url_list = ["http://myanimelist.net/anime.php?o=9&c[]=a&c[]=d&cv=2&w=1"]
-    anime_title = []
-    for url in url_list:
-        req = urllib2.Request(url, headers={'User-Agent': ""})
-        html_req = urllib2.urlopen(req)
-        html_source = html_req.readlines()
-
-        for line in html_source:
-            print line
